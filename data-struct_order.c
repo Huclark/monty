666654f132dc2203fcb2b988accf2e734f3fa5d6
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-11-16 21:58:26 +0100

    file updated

--- data-struct_order.c
+++ data-struct_order.c
@@ -5,6 +5,7 @@
  * @stack: A pointer to a pointer to the head of the stack_t list
  * @line_number: Line number processed
 */
+
 void queue(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
